661b0edae48f939f2b05a196338322d59abc3210
Isaac Exposito <fortiscadere03@gmail.com>
2023-10-01 16:35:27 -0500

    styling

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button'
 import "./Navbar.css"
   
 function NavigationBar() {
@@ -9,8 +10,12 @@ function NavigationBar() {
     <Navbar className="custom-navbar">
         <Container>
             <Navbar.Brand className="text-white nav" bg="primary" href="/">Our website name</Navbar.Brand>
-            <Nav.Link className="text-white nav" href="/">Home</Nav.Link>
-            <Nav.Link className="text-white nav" href="/post-listing">Post</Nav.Link>
+            <Nav className="align-items-center">
+                <Nav.Link className="text-white nav" href="/">Home</Nav.Link>
+                <Nav.Item>
+                    <Button variant="primary" className="text-white nav bg-primary" href="/post-listing" id="post-button">Post Listing</Button>
+                </Nav.Item>
+            </Nav>
         </Container>
     </Navbar>
   );
